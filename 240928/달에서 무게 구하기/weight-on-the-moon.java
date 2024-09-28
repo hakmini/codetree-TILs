63810cf30f86d04891b5af38ee5108b54041763b
hakmind <102711058+hakmini@users.noreply.github.com>
2024-09-28 17:42:30 +0900

    Title: 달에서 무게 구하기, Time: 88ms, Memory: 9MB, Status: Passed - CodeTree

--- 240928/달에서 무게 구하기/weight-on-the-moon.java
+++ 240928/달에서 무게 구하기/weight-on-the-moon.java
@@ -3,10 +3,7 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         int a = 13;
         double b = 0.165;
-        double c = a * b;
-        System.out.println(a + " * ");
-        System.out.println("%.6f", b);
-        System.out.println(" = ");
-        System.out.print("%.6f", c);
+
+        System.out.printf("%d * %.6f = %.6f", a, b, a*b);
     }
 }
