6efa145a020f1369524f215c3fa0865920b716c1
hakmind <102711058+hakmini@users.noreply.github.com>
2024-09-28 17:40:45 +0900

    Title: 달에서 무게 구하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240928/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int a = 13;
+        double b = 0.165;
+        double c = a * b;
+        System.out.println(a + " * ");
+        System.out.println("%.6f", b);
+        System.out.println(" = ");
+        System.out.print("%.6f", c);
+    }
+}
