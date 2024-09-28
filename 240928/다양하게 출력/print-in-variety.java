6deb2e017ab01c91c68949f2e31a072756a0e5bd
hakmind <102711058+hakmini@users.noreply.github.com>
2024-09-28 15:44:36 +0900

    Title: 다양하게 출력, Time: 94ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240928/다양하게 출력/print-in-variety.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Total days in Year");
+        System.out.print("365");
+        System.out.print("Circumference rate");
+        System.out.print("3.1415926535");
+    }
+}
