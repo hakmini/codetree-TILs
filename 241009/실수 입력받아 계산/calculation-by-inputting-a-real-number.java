3453c7fa0c47d44bb9b969ab2564f12dd1e29c1c
hakmind <102711058+hakmini@users.noreply.github.com>
2024-10-09 23:04:06 +0900

    Title: 실수 입력받아 계산, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241009/실수 입력받아 계산/calculation-by-inputting-a-real-number.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        double a = nextDouble();
+        double b = nextDouble();
+        double sum = a+b;
+
+        System.out.print("%.2f", sum);
+
+    }
+}
