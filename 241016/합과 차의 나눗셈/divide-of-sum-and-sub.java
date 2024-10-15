51f305ddfd41fa6b9d1db0684e989a0a9f68f803
hakmind <102711058+hakmini@users.noreply.github.com>
2024-10-16 00:36:30 +0900

    Title: 합과 차의 나눗셈, Time: 100ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 241016/합과 차의 나눗셈/divide-of-sum-and-sub.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        double a = 10;
+        double b = 3;
+        double c = a+b;
+        double d = a-b;
+
+        System.out.printf("%.2f", c/d );
+    }
+}
