6a86943e73a70061a8890304455dcba0585c39c7
hakmind <102711058+hakmini@users.noreply.github.com>
2025-01-03 17:45:52 +0900

    Title: 시력 검사 2, Time: 142ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 250103/시력 검사 2/eye-test-2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        double eye = sc.nextDouble();
+        if(eye >= 1.0){
+            System.out.println("High");
+        }
+        else if(eye >= 0.5){
+            System.out.println("Middle");
+        }
+        else{
+            System.out.println("Low");
+        }
+    }
+}
