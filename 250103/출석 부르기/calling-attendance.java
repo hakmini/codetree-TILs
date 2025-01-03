e5c6d72b88be9ec59777f905650cd66b21f88c01
hakmind <102711058+hakmini@users.noreply.github.com>
2025-01-03 18:05:27 +0900

    Title: 출석 부르기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 250103/출석 부르기/calling-attendance.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int at = sc.nextInt();
+        if(at = 1){
+            System.out.println("John");
+        }else if(at = 2){
+            System.out.println("Tom");
+        }else if(at = 3){
+            System.out.println("Paul");
+            System.out.println("Vacancy");
+        }
+    }
+}
