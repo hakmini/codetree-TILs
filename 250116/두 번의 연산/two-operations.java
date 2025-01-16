7d1b6988b662d48ca84b7ad81bcb35dc07df6e2d
hakmind <102711058+hakmini@users.noreply.github.com>
2025-01-16 19:01:40 +0900

    Title: 두 번의 연산, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

--- 250116/두 번의 연산/two-operations.java
+++ 250116/두 번의 연산/two-operations.java
@@ -4,17 +4,16 @@ public class Main {
         // Please write your code here.
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        if(a % 2 == 0){
-            System.out.println(a);
-        }
-
-        if(a % 2 == 1){
+        if (a % 2 == 1) {
             a += 3;
         }
 
-        if(a % 3 == 0){
-            System.out.println(a/3);
+        // a가 3의 배수라면 3으로 나누기
+        if (a % 3 == 0) {
+            a /= 3;
         }
+         System.out.println(a);
+
 
 
 
