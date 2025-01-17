3097bb8a937ca7d61c1865da8691d3fcbce7e6de
hmca0203 <102711058+hakmini@users.noreply.github.com>
2025-01-17 14:06:23 +0900

    Title: 점수 비교, Time: 136ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250117/점수 비교/score-comparison.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int Am = sc.nextInt();
+        int Ae = sc.nextInt();
+        int Bm = sc.nextInt();
+        int Be = sc.nextInt();
+
+        if(Am >= Bm && Ae >= Be){
+            System.out.println("1");
+        }else{
+            System.out.println("0");
+        }
+
+
+
+
+        
+    }
+}
