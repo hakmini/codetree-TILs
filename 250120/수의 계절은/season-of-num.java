c1ad1e0b9d3e9e02b468505d301439a7af84821a
hmca0203 <102711058+hakmini@users.noreply.github.com>
2025-01-20 17:14:59 +0900

    Title: 수의 계절은, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

--- 250120/수의 계절은/season-of-num.java
+++ 250120/수의 계절은/season-of-num.java
@@ -4,11 +4,11 @@ public class Main {
         // Please write your code here.
         Scanner sc = new Scanner(System.in);
         int m = sc.nextInt();
-        if(m = 3 || m = 4 || m = 5){
+        if(m == 3 || m == 4 || m == 5){
             System.out.println("Spring");
-        }else if(m = 6 || m = 7 || m = 8){
+        }else if(m == 6 || m == 7 || m == 8){
             System.out.println("Summer");
-        }else if(m = 9 || m = 10 || m = 11){
+        }else if(m == 9 || m == 10 || m == 11){
             System.out.println("Fall");
         }else{
             System.out.println("Winter");
