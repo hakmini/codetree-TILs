ef8d445f8ee3633373016cfc13abf908ee7a0e1b
hmca0203 <102711058+hakmini@users.noreply.github.com>
2025-01-20 19:25:23 +0900

    Title: 최댓값 구하기, Time: 133ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 250120/최댓값 구하기/maximum-value.java
+++ 250120/최댓값 구하기/maximum-value.java
@@ -9,7 +9,7 @@ public class Main {
 
         if(a > b && a > c){
             System.out.println(a);
-        }else if(b > a %% b > c){
+        }else if(b > a && b > c){
             System.out.println(b);            
         }else{
             System.out.println(c);
