f9cd0db5849cc281afd02137b3cd597fc5a7a758
hmca0203 <102711058+hakmini@users.noreply.github.com>
2025-01-20 17:22:42 +0900

    Title: 장학금, Time: 128ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250120/장학금/scholarship.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int f = sc.nextInt();
+
+        if(m >= 90){
+            if(f >= 95){
+                System.out.println("100000");
+            }else if(f >= 90){
+                System.out.println("50000");
+            }else{
+                System.out.println("0");
+            }
+        }
+        if(m <= 80){
+            System.out.println("0");
+        }
+       
+    }
+}
