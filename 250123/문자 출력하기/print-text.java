7d93ff07881d292590c97c5854631957c3a4a7d9
hmca0203 <102711058+hakmini@users.noreply.github.com>
2025-01-23 13:24:18 +0900

    Title: 문자 출력하기, Time: 116ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250123/문자 출력하기/print-text.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        char i = sc.next().charAt(0);
+
+        for(int n = 0; n <= 7; n++){
+            System.out.print(i);
+        }
+       
+    }
+}
