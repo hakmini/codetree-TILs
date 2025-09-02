aaa659c0b551cb459c5f2c0df8fd74ae1621eda1
hmca0203 <102711058+hakmini@users.noreply.github.com>
2025-09-02 19:20:48 +0900

    Title: 999 또는 -999, Time: 126ms, Memory: 10MB, Status: Passed - Codetree

--- 250902/999 또는 -999/999-or-999.java
+++ 250902/999 또는 -999/999-or-999.java
@@ -4,22 +4,21 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
-        int arr[] = new int[n];
+        // 배열을 구현합니다.
+        int[] arr = new int[100];
 
-        for(int i = 0; i < n; i++){
+        // 999나 -999가 주어지기 전까지 100개의 정수를 입력받습니다.
+        for (int i = 0; i < 100; i++) {
             arr[i] = sc.nextInt();
-            if(arr[i] == 999 || arr[i] == -999){
+            if (arr[i] == 999 || arr[i] == -999)
                 break;
-            }
         }
-
 
         int maxVal = arr[0];
         int minVal = arr[0];
 
         // 주어진 숫자들 중 최댓값과 최솟값을 구합니다.
-        for (int i = 1; i < n; i++) {
+        for (int i = 1; i < 100; i++) {
             if (arr[i] == 999 || arr[i] == -999)
                 break;
 
@@ -29,8 +28,9 @@ public class Main {
             if (arr[i] < minVal)
                 minVal = arr[i];
         }
-        System.out.print(maxVal + " " + minVal);
 
-
+        // 최댓값과 최솟값을 출력합니다.
+        System.out.println(maxVal + " " + minVal);
     }
 }
+
