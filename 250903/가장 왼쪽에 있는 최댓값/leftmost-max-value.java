f54fa71fae7a06c7d997ce006ffba22c9c5ca0eb
hmca0203 <102711058+hakmini@users.noreply.github.com>
2025-09-03 19:03:29 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 126ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250903/가장 왼쪽에 있는 최댓값/leftmost-max-value.java
+++ 250903/가장 왼쪽에 있는 최댓값/leftmost-max-value.java
@@ -35,10 +35,22 @@ public class Main {
 
         System.out.print(idx+1 + " " + (idx2+1) + " ");
 
-        for(int i = 0; i < idx2; i++){
-            if(arr[i] >= max3){
-                System.out.print(i+1);
-            } 
+        int end = idx2; // 이제 idx2의 왼쪽 구간을 탐색 시작
+        while (end > 0) {
+            int maxNow = arr[0];
+            int idxNow = 0;
+
+            for (int i = 1; i < end; i++) {
+                if (arr[i] > maxNow) {
+                    maxNow = arr[i];
+                    idxNow = i;
+                }
+            }
+
+            System.out.print((idxNow+1));
+
+            if (idxNow == 0) break; // 첫 번째 원소 도달 시 종료
+            end = idxNow; // 그 왼쪽 구간으로 탐색 범위 축소
         }
         
 
