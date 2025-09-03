6e65992600be8724f77d9996c7864709fff10059
hmca0203 <102711058+hakmini@users.noreply.github.com>
2025-09-03 20:10:22 +0900

    Title: 500 근처의 수, Time: 128ms, Memory: 10MB, Status: Passed - Codetree

--- 250903/500 근처의 수/near-500.java
+++ 250903/500 근처의 수/near-500.java
@@ -7,23 +7,25 @@ public class Main {
         int arr[] = new int[10];
         int arr_down[] = new int[10];
         int arr_up[] = new int[10];
+        int upCount = 0;
+        int downCount = 0;
 
         for(int i = 0; i < 10; i++){
             arr[i] = sc.nextInt();
             if(arr[i] < 500){
-                arr_down[i] = arr[i];
+                arr_down[downCount++] = arr[i];
             }
 
             if(arr[i] > 500){
-                arr_up[i] = arr[i];
+                arr_up[upCount++] = arr[i];
             }
         }
 
-        int down_max = arr_down[0];
-        int up_min = 1000;
+        int down_max = arr_down[0]; // 500보다 작은 수 중 최대
+        int up_min = arr_up[0]; // 500보다 큰 수 중 최소
 
-        for(int i = 1; i < 10; i++){
-            if(arr_down[i] != 0 && down_max < arr_down[i]){
+        for(int i = 0; i < 10; i++){
+            if(down_max < arr_down[i]){
                 down_max = arr_down[i];
             }
             if(arr_up[i] != 0 && up_min > arr_up[i]){
@@ -32,6 +34,5 @@ public class Main {
         }
 
         System.out.print(down_max + " " + up_min);
-
     }
 }
