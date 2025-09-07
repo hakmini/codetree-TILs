815a2e51692005d1638e83c6ca9a346acc5985a7
hmca0203 <102711058+hakmini@users.noreply.github.com>
2025-09-07 16:49:29 +0900

    Title: 두 수의 차의 최솟값, Time: 135ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250907/두 수의 차의 최솟값/minimum-difference-between-two-numbers.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        for(int i = 0; i < n; i++){
+            for(int j = i+1; j < n; j++){
+                if(arr[i] > arr[j]){
+                    int tmp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = tmp;
+                }
+            }
+        }
+
+  
+
+        int min = 100;
+
+        for(int i = 0; i < n-1; i++){
+            int diff = arr[i+1] - arr[i]; 
+            if(diff < min){
+                min = diff;
+            }
+        }
+
+         System.out.println(min);
+
+
+    }
+}
