e3af537e71984fc3e47aed2c0a5c4ee50e699851
hmca0203 <102711058+hakmini@users.noreply.github.com>
2025-09-07 17:31:47 +0900

    Title: 대문자로 바꾸기, Time: 117ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250907/대문자로 바꾸기/change-to-capital.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char arr[][] = new char[5][3];
+
+        for(int i = 0; i < 5; i++){
+            for(int j = 0; j < 3; j++){
+                arr[i][j] = sc.next().charAt(0);
+            }
+        }
+        
+
+
+        for(int i = 0; i < 5; i++){
+            for(int j = 0; j < 3; j++){
+                char ch = arr[i][j];
+                char upper = Character.toUpperCase(ch);
+                System.out.print(upper + " ");
+            }
+            System.out.println();  
+        }
+    }
+}
+
+
