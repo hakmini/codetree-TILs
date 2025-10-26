1743f83f21054deeb263a6fe807a9a21e307fd2f
hmca0203 <102711058+hakmini@users.noreply.github.com>
2025-10-26 23:27:15 +0900

    Title: 문자 제거하기, Time: 126ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 251026/문자 제거하기/del-char.java
+++ 251026/문자 제거하기/del-char.java
@@ -5,28 +5,36 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         String str = sc.next();
-        int len = str.length();
 
+        // 첫 번째 삭제
         int a = sc.nextInt();
-        str = str.substring(0,a) + str.substring(a+1);
-        System.out.println(str);
-
-        int b = sc.nextInt();
-        str = str.substring(0,b) + str.substring(b+1);
-        System.out.println(str);
-
-        int c = sc.nextInt();
-        str = str.substring(0,c) + str.substring(c+1);
-        System.out.println(str);
-
-        int d = sc.nextInt();
-
-        if(d > len){
-            str = str.substring(0,1);
-            System.out.println(str);
-        }else{
-            str = str.substring(0,d) + str.substring(d+1);    
-            System.out.println(str);
+        if (a >= 0 && a < str.length()) {
+            str = str.substring(0, a) + str.substring(a + 1);
         }
+        System.out.println(str);
+
+        // 두 번째 삭제
+        int b = sc.nextInt();
+        if (b >= 0 && b < str.length()) {
+            str = str.substring(0, b) + str.substring(b + 1);
+        }
+        System.out.println(str);
+
+        // 세 번째 삭제
+        int c = sc.nextInt();
+        if (c >= 0 && c < str.length()) {
+            str = str.substring(0, c) + str.substring(c + 1);
+        }
+        System.out.println(str);
+
+        // 네 번째 삭제
+        int d = sc.nextInt();
+        if (d >= 0 && d < str.length()) {
+            str = str.substring(0, d) + str.substring(d + 1);
+        } else {
+            // d가 범위를 벗어나면 첫 문자만 남기기
+            str = str.substring(0, 1);
+        }
+        System.out.println(str);
     }
 }
