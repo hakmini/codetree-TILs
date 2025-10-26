e6c7997f126e48975f50f0e5363a5ef8266117bf
hmca0203 <102711058+hakmini@users.noreply.github.com>
2025-10-26 22:58:07 +0900

    Title: 양옆 문자 제거하기, Time: 120ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251026/양옆 문자 제거하기/del-both-side-char.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+
+
+        int len = str.length();
+
+     
+        str = str.substring(0, 1) + str.substring(2);
+        len--;
+
+
+        str = str.substring(0, len - 2) + str.substring(len - 1);
+        len--;
+            
+        System.out.println(str);
+    }
+}
