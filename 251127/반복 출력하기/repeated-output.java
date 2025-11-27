77ee62c6c5d352afe15b9622027c36222171c658
hmca0203 <102711058+hakmini@users.noreply.github.com>
2025-11-27 18:32:58 +0900

    Title: 반복 출력하기, Time: 128ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251127/반복 출력하기/repeated-output.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void printing(int n){
+        
+            for(int  j = 0; j < n; j++){
+                System.out.println("12345^&*()_");
+
+            }
+           
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+
+        printing(n);
+
+
+
+    }
+}
