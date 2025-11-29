36679b1ec28dba11f343fb2a941a44ba4cb4ed5c
hmca0203 <102711058+hakmini@users.noreply.github.com>
2025-11-29 15:31:08 +0900

    Title: 정수의 최솟값, Time: 127ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251129/정수의 최솟값/minimum-value-of-an-integer.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int min(int z, int y, int x){
+        int n = 0;
+        for(int i = 0; i < 3; i++){
+            if(z > y){
+                n = y;
+            }else if(y > z){
+                n = z;
+            }
+
+
+        }
+        return n;
+    }
+
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int minmin = min(a,b, c);
+        System.out.print(minmin);
+    }
+}
