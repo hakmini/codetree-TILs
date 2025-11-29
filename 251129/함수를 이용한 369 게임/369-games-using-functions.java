8977e9fab1815a7bdaa8c82b9c4be40ecac3efa7
hmca0203 <102711058+hakmini@users.noreply.github.com>
2025-11-29 16:57:32 +0900

    Title: 함수를 이용한 369 게임, Time: 146ms, Memory: 10MB, Status: Passed - Codetree

--- 251129/함수를 이용한 369 게임/369-games-using-functions.java
+++ 251129/함수를 이용한 369 게임/369-games-using-functions.java
@@ -1,32 +1,36 @@
 import java.util.Scanner;
 
 public class Main {
-
-    // 1) 3의 배수인가?
-    public static boolean three(int n) {
-        return n % 3 == 0;
-    }
-
-    // 2) 일의 자리가 3, 6, 9인가?
-    public static boolean tsn(int n) {
-        int last = n % 10;
-        return last == 3 || last == 6 || last == 9;
-    }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int A = sc.nextInt();
-        int B = sc.nextInt();
-
-        int count = 0;
-
-        for(int i = A; i <= B; i++){
-            if (three(i) || tsn(i)) {  
-                count++;
-            }
+    // 3, 6, 9 숫자가 
+    // 단 하나라도 포함되었는지를 확인합니다.
+    public static boolean contains369(int n) {
+        // 계속 10으로 나눠주며
+        // 일의 자리를 조사합니다.
+        while(n > 0) {
+            if(n % 10 == 3 || n % 10 == 6 || n % 10 == 9)
+                return true;
+            n /= 10;
         }
 
-        System.out.println(count);
+        return false;
+    }
+
+    // 3, 6, 9를 포함하거나 3의 배수인지를 판단합니다.
+    public static boolean is369Number(int n) {
+        return contains369(n) || (n % 3 == 0);
+    }
+    
+    public static void main(String[] args) {
+        // 변수 선언 및 입력:
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int cnt = 0;
+        for(int i = a; i <= b; i++)
+            if(is369Number(i))
+                cnt++;
+
+        System.out.println(cnt);
     }
 }
