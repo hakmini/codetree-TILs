7d7be25835a36478f19800884cfd7350c265c6bd
hmca0203 <102711058+hakmini@users.noreply.github.com>
2025-12-02 17:07:19 +0900

    Title: 2021년 날짜의 유무, Time: 127ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251202/2021년 날짜의 유무/with-or-without-2021.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static boolean isDay(int m, int d) {
+        // 월 범위 체크
+        if (m < 1 || m > 12)
+            return false;
+
+        // 월별 일수
+        int[] days = {0,31,28,31,30,31,30,31,31,30,31,30,31};
+
+        // 일 범위 체크
+        if (d < 1 || d > days[m])
+            return false;
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int d = sc.nextInt();
+
+        if(isDay(m, d))
+            System.out.print("Yes");
+        else
+            System.out.print("No");
+    }
+}
