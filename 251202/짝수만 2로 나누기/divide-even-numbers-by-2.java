134956864e3e95bd705a1d141f5379bab77f3432
hmca0203 <102711058+hakmini@users.noreply.github.com>
2025-12-02 17:52:49 +0900

    Title: 짝수만 2로 나누기, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251202/짝수만 2로 나누기/divide-even-numbers-by-2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Main {
+    public static void modify(int arr2[], int n){
+        for(int i = 0; i < n; i++){
+            if(arr2[i] % 2 == 0){
+                arr2[i] = arr2[i] / 2;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+
+        modify(arr, n);
+
+        for(int i = 0; i < n; i++){
+            System.out.print(arr[i] + " ");
+        }
+
+
+    }
+}
