9d04b7ff9f172936a576c62c28548e704b9be92a
hmca0203 <102711058+hakmini@users.noreply.github.com>
2025-12-04 17:31:09 +0900

    Title: 두 정수에 대한 연산값 2, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251204/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+class mainPro{
+    int value;
+    public mainPro(int value){
+        this.value = value;
+    }
+}
+
+public class Main {
+    public static void modify(mainPro n , mainPro m){
+        if(n.value < m.value){
+            n.value = n.value + 10;
+            m.value = m.value * 2;
+        }else{
+            m.value = m.value + 10;
+            n.value = n.value * 2;            
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        // Please write your code here.
+        mainPro n = new mainPro(a);
+        mainPro m = new mainPro(b);
+
+        modify(n,m);
+
+        System.out.print(n.value + " " + m.value);
+
+    }
+}
