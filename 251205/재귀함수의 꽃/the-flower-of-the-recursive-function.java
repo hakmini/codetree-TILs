2a697ee45d4773ef5ec1507bf8df6fc9b5ae5902
hmca0203 <102711058+hakmini@users.noreply.github.com>
2025-12-05 18:59:13 +0900

    Title: 재귀함수의 꽃, Time: 136ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251205/재귀함수의 꽃/the-flower-of-the-recursive-function.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {    
+    // 재귀함수를 이용해 숫자를 n부터 1까지 출력하고, 다시 1부터 n까지 출력합니다.
+    public static void printStar(int n) {
+        if(n == 0)
+            return;
+    
+        System.out.print(n + " ");
+        printStar(n - 1);
+        System.out.print(n + " ");
+    }
+    
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 변수 선언 및 입력:
+        int n = sc.nextInt();
+
+        printStar(n);
+    }
+}
