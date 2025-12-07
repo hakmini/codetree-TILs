689a50795731cf3300582667d7d7807c04dbb0ff
hmca0203 <102711058+hakmini@users.noreply.github.com>
2025-12-07 17:32:30 +0900

    Title: 재귀함수를 이용한 3N + 1 수열, Time: 120ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251207/재귀함수를 이용한 3N + 1 수열/3n-plus-1-sequence-with-recursive-function.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main {
+    public static int count;
+
+    public static int F(int n){
+        if(n == 1) return count;
+
+        if(n % 2 == 0){
+            count++;
+            return F(n / 2);
+        }else{
+            count++;
+            return F(n * 3 + 1);
+        }
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // Please write your code here.
+        System.out.print(F(n));
+    }
+}
