3b3a1aaf978a7c1391ae8031fc9cf22ebecd59ee
hmca0203 <102711058+hakmini@users.noreply.github.com>
2025-12-08 19:59:06 +0900

    Title: k번째로 신기한 문자열, Time: 139ms, Memory: 10MB, Status: Passed - Codetree

--- 251208/k번째로 신기한 문자열/kth-special-string.java
+++ 251208/k번째로 신기한 문자열/kth-special-string.java
@@ -7,26 +7,22 @@ public class Main {
         int n = sc.nextInt();
         int k = sc.nextInt();
 
-        String t = sc.next();  // 접두사 문자열 T
+        String t = sc.next(); 
 
         String[] words = new String[n];
         for (int i = 0; i < n; i++) {
             words[i] = sc.next();
         }
 
-        // T로 시작하는 단어만 리스트에 담기
         List<String> list = new ArrayList<>();
-
+        
         for (int i = 0; i < n; i++) {
             if (words[i].startsWith(t)) {
                 list.add(words[i]);
             }
         }
-
-        // 사전순 정렬
         Collections.sort(list);
 
-        // K번째 출력
         System.out.println(list.get(k - 1));
     }
 }
