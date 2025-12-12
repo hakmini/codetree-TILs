b6426576269f4d4080267c9aa84999ecb614a5fb
hmca0203 <102711058+hakmini@users.noreply.github.com>
2025-12-12 16:28:08 +0900

    Title: 진수 to 진수, Time: 129ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251212/진수 to 진수/transformation-of-number-system.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();  
+        int B = sc.nextInt();   
+        String N = sc.next();   
+  
+        int num = 0;
+        for (int i = 0; i < N.length(); i++) {
+            num = num * A + (N.charAt(i) - '0');
+        }
+
+
+        int[] result = new int[100];
+        int cnt = 0;
+
+        while (num > 0) {
+            result[cnt++] = num % B;
+            num /= B;
+        }
+
+        for (int i = cnt - 1; i >= 0; i--) {
+            System.out.print(result[i]);
+        }
+    }
+}
